f963ef74837323d036a1be1b93e1f1f4858e2c54
Brandon Jones <bdjones2410@gmail.com>
2015-10-19 15:11:51 -0400

    reformatted to follow guidelines better, making divs a class

--- main.js
+++ main.js
@@ -1,37 +1,4 @@
-//idea to make it into a prompt for the user to change credit to Max Krause
 
-var readSpeed = prompt("What is your average word per minute read speed? global average is 230");
-  function makesure() {
-  if(isNaN(readSpeed)) {
-    readSpeed = 230;
-    return readSpeed
-  }
-  else {
-    return readSpeed
-  }
-};
-makesure();
+getCount('first','time-to-read');
 
-function WPM(things) {
-  var wordsPer = Math.round(things.length/readSpeed);
-  return wordsPer
-}
-
-function getCount (sectionNum, writeTo){
-var roughArray = document.getElementById(sectionNum).textContent.split(" ");
-
-//Thank you Paul Draper(stackOverflow) for this method of removing the white spaces from my array.
-
-roughArray = roughArray.filter(function(str){
-  return /\S/.test(str);
-});
-
-
-var timeToRead = WPM(document.getElementById(sectionNum).textContent.split(" "));
-document.getElementById(writeTo).innerHTML = timeToRead + " minutes to read";
-}
-
-
-getCount('first','firstPerMin');
-
-getCount('second','secondPerMin');
+getCount('second','time-to-read2');

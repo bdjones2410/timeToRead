407f3de442367f8f5cf1d057ad091d15fa6bc300
Brandon Jones <bdjones2410@gmail.com>
2015-10-20 08:50:56 -0400

    commented out the user choice, made readSpeed set variable

--- readTime.js
+++ readTime.js
@@ -1,22 +1,24 @@
-//idea to make it into a prompt for the user to change credit to Max Krause
+// //idea to make it into a prompt for the user to change credit to Max Krause
+//
+// var readSpeed = prompt("What is your average word per minute read speed? global average is 230");
+//   function makesure() {
+//   if(isNaN(readSpeed)) {
+//     readSpeed = 230;
+//     return readSpeed
+//   }
+//   // thank you jfriend00 on stackOverFlow for showing what the value of a no input prompt equates to.
+//
+//   else if (readSpeed === "") {
+//     readSpeed = 230;
+//     return readSpeed
+//   }
+//   else {
+//     return readSpeed
+//   }
+// };
+// makesure();
 
-var readSpeed = prompt("What is your average word per minute read speed? global average is 230");
-  function makesure() {
-  if(isNaN(readSpeed)) {
-    readSpeed = 230;
-    return readSpeed
-  }
-  // thank you jfriend00 on stackOverFlow for showing what the value of a no input prompt equates to.
-  
-  else if (readSpeed === "") {
-    readSpeed = 230;
-    return readSpeed
-  }
-  else {
-    return readSpeed
-  }
-};
-makesure();
+var readSpeed = 230;
 
 function WPM(things) {
   var wordsPer = Math.round(things.length/readSpeed);
